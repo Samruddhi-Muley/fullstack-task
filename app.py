7ec0b0b5bb5a61e64895d896a1ca45e01eedb86b
Samruddhi-Muley <samruddhimuley139@gmail.com>
2025-12-20 02:27:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,6 @@ app.register_blueprint(public_bp)
 app.register_blueprint(admin_bp, url_prefix="/admin")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
